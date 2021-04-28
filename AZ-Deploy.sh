23a80caf26ba9e479c48c4f69b295b7411216944
CaptainBrooksie <captainbrooksie@gmail.com>
2021-04-28 15:26:46 +0100

    Update AZ-Deploy.sh

--- AZ-Deploy.sh
+++ AZ-Deploy.sh
@@ -2,4 +2,4 @@
 
 az group create --name MinecraftRG --location westeurope
 
-az vm create --resource-group Minecraft --name MinecraftVM --size Standard_B1s --image win2016datacenter --location westeurope --admin-username azureuser
+az vm create --resource-group MinecraftRG --name MinecraftVM --size Standard_B1s --image win2016datacenter --location westeurope --admin-username azureuser
