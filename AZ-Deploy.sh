4ac75a92d054dab44a69c91c34272d115cc634c7
CaptainBrooksie <captainbrooksie@gmail.com>
2021-04-28 13:17:24 +0100

    Update AZ-Deploy.sh

--- AZ-Deploy.sh
+++ AZ-Deploy.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 # Generate a unique suffix for the service name
-let randomNum=$RANDOM
+#let randomNum=$RANDOM
 
 # Generate a unique service and group name with the suffix
-ServiceName=Minecraft$randomNum
+#ServiceName=Minecraft$randomNum
 
 #resources name must be lowercase
-myResourceGroupName=$ServiceName"Group"
-myStorageAccountName=$ServiceName"Storage"
-myVMName=$ServiceName"VM"
+#myResourceGroupName=$ServiceName"Group"
+#myStorageAccountName=$ServiceName"Storage"
+#myVMName=$ServiceName"VM"
 
 # Create a resource group.
-az group create --name $myResourceGroupName --location westeurope
+az group create --name Minecraft-RG --location westeurope
 
 # Create a general-purpose storage account in your resource group.
 az storage account create \
-    --resource-group $myResourceGroupName \
-    --name $myStorageAccountName \
+    --resource-group Minecraft-RG \
+    --name Minecraft-SA \
     --location westeurope \
     --sku Standard_LRS
 
 # Create Windowd Virtual Machine
 
 az vm create \
-    --resource-group $myResourceGroup \
-    --name $myVMName \
+    --resource-group Minecraft-RG \
+    --name MinecraftVM \
     --image win2019datacenter \
     --admin-username azureuser \
     --location westeurope \
