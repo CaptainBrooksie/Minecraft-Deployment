86acca2ab5ab60f7ee50de8d4ebb13c8ac7dedb2
CaptainBrooksie <captainbrooksie@gmail.com>
2021-04-28 15:22:18 +0100

    Update AZ-Deploy.sh

--- AZ-Deploy.sh
+++ AZ-Deploy.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Generate a unique suffix for the service name
-let randomNum=$RANDOM*$RANDOM
-
-# Generate a unique service and group name with the suffix
-SignalRName=Minecraft$randomNum
-#resource name must be lowercase
-mySignalRSvcName=${SignalRName,,}
-myResourceGroupName=SignalRName"Group"
-myVM=$SignalRName"VM"
+myResourceGroupName=Minecraft
+myVM=MinecraftVM$RANDOM
 
 az group create --name $myResourceGroupName --location westeurope
 
