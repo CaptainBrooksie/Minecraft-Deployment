a2035b740eb5e75f2b9e636f9c955f8891fff054
CaptainBrooksie <captainbrooksie@gmail.com>
2021-04-28 13:53:44 +0100

    Update AZ-Deploy.sh

--- AZ-Deploy.sh
+++ AZ-Deploy.sh
@@ -4,4 +4,4 @@ az group create --name MinecraftRG --location westeurope
 
 az storage account create --name MinecraftSA --resource-group MinecraftRG --location westeurope --sku Standard_LRS
 
-az vm create --resource-group MinecraftRG --name MinecraftVM --image win2019datacenter --admin-username azureuser --location westeurope --size Standard_B1s
+az vm create --resource-group MinecraftRG --name MinecraftVM --image win2019datacenter --admin-username azureuser --location westeurope --size Standard_B1s --public-ip-address --public-ip-address-allocation dynamic --public-ip-address-dns-name
