c19c017353a0af3488851d4e26fe4fe553bfc5ee
CaptainBrooksie <captainbrooksie@gmail.com>
2021-04-28 11:15:16 +0100

    Add files via upload

---
+++ AZ-Deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Generate a unique suffix for the service name
+let randomNum=$RANDOM*$RANDOM
+
+# Generate a unique service and group name with the suffix
+ServiceName=MinecraftSvc$randomNum
+
+#resources name must be lowercase
+myResourceGroupName=$ServiceName"Group"
+myStorageAccountName=$ServiceName"Storage"
+myVMName=$ServiceName"VM"
+
+# Create a resource group.
+az group create --name $myResourceGroupName --location westeurope
+
+# Create a general-purpose storage account in your resource group.
+az storage account create \
+    --resource-group $myResourceGroupName \
+    --name $myStorageAccountName \
+    --location westeurope \
+    --sku Standard_LRS
+
+# Create Windowd Virtual Machine
+
+az vm create \
+    --resource-group $myResourceGroup \
+    --name $myVMName \
+    --image win2019datacenter \
+    --admin-username azureuser \
+    --location westeurope \
+    --size B1s
